1591f56e8d51dd8461d7792a9f320803a2113249
codecracker2014 <raaz2.gajendra@gmail.com>
2016-07-06 00:45:41 +0530

    version  1release correction

--- www/js/controllers/planningController.js
+++ www/js/controllers/planningController.js
@@ -1,6 +1,5 @@
 angular.module('starter.controllers')
 
-<<<<<<< HEAD
 .controller('planningController', function($scope,planningService,$cordovaContacts,notificationService,$ionicSideMenuDelegate) {
 
 
@@ -24,19 +23,4 @@ $scope.toggleLeft = function() {
 
  };
 
-=======
-.controller('planningController', function($scope,planningService,$ionicActionSheet, $timeout,$ionicSideMenuDelegate) {
-
-   $scope.toggleLeft = function() {
-    $ionicSideMenuDelegate.toggleLeft();
-    console.log("Called left");
-    
-  };
-  
-  $scope.addEvent=function()
-  {
-    
-  };
-  
->>>>>>> 64cfcf48d717ff02916c9f4570b85c864e521c18
 })
