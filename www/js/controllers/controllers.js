786f332cdd595c0c66d1362b16845a6dad8d090f
codecracker2014 <raaz2.gajendra@gmail.com>
2016-07-06 03:17:33 +0530

    oneZone datepicker added

--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -9,75 +9,95 @@ console.log("L");
 	$scope.currentDate = dao.date;
 	$scope.month=month_name(dao.date.getMonth(),1);
 	$scope.level=dao.level;
+
 	console.log("month"+$scope.month);
+	$scope.onezoneDatepicker = {
+	    date: $scope.currentDate,
+	    mondayFirst: false,
+	    disablePastDays: false,
+	    disableSwipe: false,
+	    disableWeekend: false,
+	    showDatepicker: false,
+	    showTodayButton: true,
+	    calendarMode: false,
+	    hideCancelButton: false,
+	    hideSetButton: false,
 
-	//$scope.minDate = new Date($scope.currentDate.getYear(), $scope.currentDate.getMonth(), 1);
-	//$scope.maxDate = new Date($scope.currentDate.getYear(), $scope.currentDate.getMonth(), 30);
-	$scope.datePickerCallback = function (val) {
-		if (!val) {
-			console.log('Date not selected');
-		} else {
+	    callback: function(val){
 
-		//$ionicSlideBoxDelegate.start();
-			AppDate.dt=val;
-			dao.date=val;
-			dao.refresh();
-			incomeService.refresh();
-			console.log(dao.today);
-			console.log(AppDate.getAppDate());
-			console.log('Selected date is : ', val);
-			$scope.month=month_name(dao.date.getMonth(),1);
+				console.log("callback"+val);
+				AppDate.dt=val;
+				dao.date=val;
+				dao.refresh();
+				incomeService.refresh();
+				console.log(dao.today);
+				console.log(AppDate.getAppDate());
+				console.log('Selected date is : ', val);
+				$scope.month=month_name(dao.date.getMonth(),1);
 
-			$ionicSlideBoxDelegate.update();
-			console.log("Updated");
-		}
+				$ionicSlideBoxDelegate.update();
+				console.log("Updated");
+
+	    }
 	};
-
-
-
-
 //Start date
+
+
+
+
 	$scope.currentDate1 = statusService.startDate;
 	$scope.month1=month_name(statusService.startDate.getMonth(),1);
 	console.log("month1"+$scope.month1);
 	//$scope.minDate = new Date($scope.currentDate.getYear(), $scope.currentDate.getMonth(), 1);
 	//$scope.maxDate = new Date($scope.currentDate.getYear(), $scope.currentDate.getMonth(), 30);
-	$scope.datePickerCallback1 = function (val) {
-		if (!val) {
-			console.log('Date not selected');
-		} else {
-			statusService.startDate=val;
-			statusService.refresh();
-			console.log('Selected date is : ', val);
-			$scope.month1=month_name(statusService.startDate.getMonth(),1);
 
-		}
+	$scope.onezoneDatepicker1 = {
+	    date: $scope.currentDate1,
+	    mondayFirst: false,
+	    disablePastDays: false,
+	    disableSwipe: false,
+	    disableWeekend: false,
+	    showDatepicker: false,
+	    showTodayButton: true,
+	    calendarMode: false,
+	    hideCancelButton: false,
+	    hideSetButton: false,
+
+	    callback: function(val){
+
+				statusService.startDate=val;
+				statusService.refresh();
+				console.log('Selected date is : ', val);
+				$scope.month1=month_name(statusService.startDate.getMonth(),1);
+
+	    }
 	};
-
 //end Date
 $scope.currentDate2 = statusService.endDate;
 $scope.month2=month_name(statusService.endDate.getMonth(),1);
 console.log("month2"+$scope.month2);
 //$scope.minDate = new Date($scope.currentDate.getYear(), $scope.currentDate.getMonth(), 1);
 //$scope.maxDate = new Date($scope.currentDate.getYear(), $scope.currentDate.getMonth(), 30);
-$scope.datePickerCallback2 = function (val) {
-	if (!val) {
-		console.log('Date not selected');
-	} else {
-		statusService.endDate=val;
-		statusService.refresh();
+$scope.onezoneDatepicker2 = {
+		date: $scope.currentDate2,
+		mondayFirst: false,
+		disablePastDays: false,
+		disableSwipe: false,
+		disableWeekend: false,
+		showDatepicker: false,
+		showTodayButton: true,
+		calendarMode: false,
+		hideCancelButton: false,
+		hideSetButton: false,
 
-		console.log('Selected date is : ', val);
-		$scope.month2=month_name(statusService.endDate.getMonth(),1);
+		callback: function(val){
 
-	}
+			statusService.endDate=val;
+			statusService.refresh();
+
+			console.log('Selected date is : ', val);
+			$scope.month2=month_name(statusService.endDate.getMonth(),1);
+
+		}
 };
-
-console.log("E");
-
 })
-
-
-
-
-//Status controller
